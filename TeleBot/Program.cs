3dde1a2a7df026c89afcd6b045e2df6bcbffab7f
marcelocampos <marcelo.camposs182@gmail.com>
2019-08-04 10:37:00 -0300

    primeiro commit
    first commit

---
+++ TeleBot/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Dynamic;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+// TeleBot - envio de msgs Telegram por http
+// versão 0.1 : Ago/2019
+// by Marcelo Campos : www.marcelocampos.cc
+// To send the “Hello World” message using a web-browser, just open the URL:
+// https://api.telegram.org/bot<TOKEN>/sendMessage?chat_id=<CHAT_ID>&text=Hello%20World
+
+namespace TeleBot
+{
+    public class Program
+    {
+
+        private static string botToken = string.Empty;
+        static TeleBotWorker teleBot;
+
+        static void Main(string[] args)
+        {
+            int timeOut = 6000;
+            int timeCount = 0;
+            int timeSleep = 50; // em milisegs
+
+            try
+            {
+
+                if (!ReadConfigs()) // lê arquivo de configurações e verifica se retornou ok (true)
+                {
+                    ConsolePrint("ERRO: Durante a Leitura do Arquivo de configurações (\"config.cfg\")\r\n", true);
+                    return;
+                }
+                else if (string.IsNullOrEmpty(botToken))
+                {
+                    ConsolePrint("ERRO: arquivo de configurações (\"config.cfg\") Não contém o Token do Bot ...\r\n", true);
+                    return;
+                }
+                else if (args.Length == 0)
+                {
+                    ConsolePrint("ERRO: Não Recebido Parâmetro de ação, tente \"Ler\" ou \"Enviar ChatId \"texto_da_mensagem\" (texto com aspas mesmo)\" ...\r\n", true);
+                    return;
+                }
+
+                teleBot = new TeleBotWorker(botToken, false);
+
+                switch (args[0].Trim().ToLower())    // verifica qual ação
+                {
+                    case "enviar":
+                        if (args.Length != 3)
+                        { ConsolePrint("ERRO: Parâmetro de ação Envio incorreto, tente: \"Enviar ChatId \"texto_da_mensagem\" (texto com aspas mesmo)\" ...\r\n", true); return; }
+
+                        TeleBotWorker.DoPost(args[1], args[2]);
+
+                        TeleBotWorker.statusEnvio = TeleBotWorker.StatusEnvio.AguardandoEnviar;
+                        while (TeleBotWorker.statusEnvio == TeleBotWorker.StatusEnvio.AguardandoEnviar)
+                        {
+                            System.Threading.Thread.Sleep(timeSleep);
+                            if (timeCount++ >= (timeOut / timeSleep))
+                            {
+                                ConsolePrint("Timeout no Envio...", false);
+                                break;
+                            }
+                        }
+                        break;
+
+                    case "receber":
+                    case "ler":
+                        TeleBotWorker.statusReceber = TeleBotWorker.StatusReceber.AguandandoReceber;
+                        ConsolePrint("Recebendo Atualizações Mensagens, Aguarde ...\r\n", false);
+                        TeleBotWorker.GetMessages();
+
+                        while (TeleBotWorker.statusReceber == TeleBotWorker.StatusReceber.AguandandoReceber)
+                        {
+                            System.Threading.Thread.Sleep(timeSleep);
+                            if (timeCount++ >= (timeOut / timeSleep))
+                            {
+                                ConsolePrint("Timeout no Recebimento...", false);
+                                break;
+                            }
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+                ConsolePrint(string.Empty, true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public static void ConsolePrint(string strToPrint, bool aguardaTeclapSair)
+        {
+            Console.Write(strToPrint);
+
+            if (aguardaTeclapSair)
+            {
+                Console.Write("\r\nPressione Qualquer tecla para sair...");
+                Console.ReadKey();
+            }
+        }
+
+        private static bool ReadConfigs()
+        {
+            bool ret = false;
+            string fileName = "config.cfg";
+
+            if (!File.Exists(fileName))
+                return false;
+
+            StreamReader configFile = new StreamReader(fileName);
+
+            try
+            {
+                while (!configFile.EndOfStream)
+                {
+                    string lineRead = configFile.ReadLine();        // lê um linha do arquivo
+                                                                    // se linha começa com xx ou não tem o '=' então pula
+                    if (lineRead.StartsWith("#") || lineRead.StartsWith(";") || !lineRead.Contains("="))
+                        continue;
+
+                    string[] paramValor = lineRead.Split('=');
+
+                    if (paramValor.Length != 2)   // tem sempre de ter o par: parâmetro e valor      
+                        return false;
+
+                    switch (paramValor[0].ToLower())
+                    {
+                        case "token":
+                            botToken = paramValor[1];
+                            break;
+                        // ... outros parâmetros ... :
+                        // case zzz :
+                        default:
+                            break;
+                    }
+                }
+
+                configFile?.Close();
+                ret = true;
+            }
+            catch (Exception)
+            {
+
+                configFile?.Close();
+            }
+
+            return ret;
+        }
+
+    }
+}
---
+++ TeleBot/TeleBotWorker.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace TeleBot
+{
+    class TeleBotWorker
+    {
+        private static string botToken;
+        private static bool consoleSilentMode;
+
+        public TeleBotWorker(string _botToken, bool _consoleSilentMode)
+        {
+            botToken = _botToken;
+            consoleSilentMode = _consoleSilentMode;
+        }
+
+        public enum StatusEnvio
+        {
+            AguardandoEnviar = -1,
+            Enviado,
+            Erro
+        };
+        public static StatusEnvio statusEnvio;
+
+        public enum StatusReceber
+        {
+            AguandandoReceber = -1,
+            RecebimentoConcluido,
+            Erro
+
+        };
+        public static StatusReceber statusReceber;
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async void GetMessages()
+        {
+            string message_id = string.Empty;
+            string first_name = string.Empty;
+            string last_name = string.Empty;
+            string username = string.Empty;
+            string chat_id = string.Empty;
+            string text = string.Empty;
+            string dtMsg;
+
+            string urlTelegramChat = "https://api.telegram.org/bot" + botToken + "/getUpdates";  // para receber  
+
+            HttpClient cliente = new HttpClient();
+            string msgHeader = "\"result\":";
+
+            string response = await cliente.GetStringAsync(urlTelegramChat);
+
+            int startPoint = response.IndexOf(msgHeader) + msgHeader.Length;
+            string TelegramMessageArray = response.Substring(startPoint);
+            TelegramMessageArray = TelegramMessageArray.Substring(0, TelegramMessageArray.Length - 1);
+
+            int arrayCount = TelegramMessageArray.Split(new string[] { "message_id" }, StringSplitOptions.None).Length - 1;
+
+            var result = (dynamic)JArray.Parse(TelegramMessageArray);
+            if (arrayCount > 0)
+            {
+                if (arrayCount == 1)
+                {
+                    if (!consoleSilentMode)
+                        Console.WriteLine("Uma nova atualização recebida:");
+                }
+
+                else
+                {
+                    if (!consoleSilentMode)
+                        Console.WriteLine(arrayCount.ToString() + " Novas atualizações recebidas:");
+                }
+
+
+                for (int i = 0; i < arrayCount; i++)
+                {
+                    message_id = (result[i].message.message_id?.ToString());
+                    first_name = (result[i].message.from.first_name?.ToString());
+                    last_name = (result[i].message.from.last_name?.ToString());
+                    username = (result[i].message.from.username?.ToString());
+
+                    chat_id = (result[i].message.chat.id?.ToString());
+
+                    text = (result[i].message.text?.ToString());
+                    dtMsg = result[i].message.date.ToString();
+
+                    // if(!consoleSilentMode) {}
+                    Program.ConsolePrint(message_id + "; " + chat_id + "; " + first_name + "; " + last_name + "; " + username + "; " + text + "\r\n", false);
+                    Program.ConsolePrint("-----------------------------------------------------" + "\r\n", false);
+                }
+            }
+            else
+            {
+                // if(!consoleSilentMode)
+                Program.ConsolePrint("Sem novas Mensagens :-(" + "\r\n", false);
+            }
+
+            statusReceber = StatusReceber.RecebimentoConcluido;
+        }
+
+        public static void DoPost(string chatId, string textMsg)
+        {
+            string urlTelegramPost = "https://api.telegram.org/bot";
+            PostAsync(urlTelegramPost + botToken + "/" + "sendMessage?chat_id=" + chatId + "&text=" + textMsg.Replace(" ", "%20"), "");
+        }
+
+        private static async void PostAsync(string uri, string data)
+        {
+            var httpClient = new HttpClient();
+
+            var getRequest = await httpClient.GetAsync(uri);
+            if (!consoleSilentMode)
+                Console.WriteLine("> PostAsync -> " + uri + "  ");
+            try
+            {
+                if (getRequest.EnsureSuccessStatusCode().IsSuccessStatusCode)
+                {
+                    string response = await getRequest.Content.ReadAsStringAsync();
+
+                    var JsonResponseDynamic = (dynamic)JObject.Parse(response);
+
+                    if (bool.Parse(JsonResponseDynamic.ok.ToString()))
+                    {
+                        if (!consoleSilentMode)
+                            Console.WriteLine("Sucesso no Envio");
+                        statusEnvio = StatusEnvio.Enviado;
+                    }
+                }
+                else
+                {
+                    if (!consoleSilentMode)
+                        Console.WriteLine("> ERRO no request, uri: \r\n" + uri);
+                    statusEnvio = StatusEnvio.Erro;
+                }
+            }
+            catch (System.Net.Http.HttpRequestException e)
+            {
+                if (!consoleSilentMode)
+                    Console.WriteLine("> ERRO no request, uri: \r\n" + uri);
+            }
+            catch (Exception e)
+            {
+                if (!consoleSilentMode)
+                    Console.WriteLine("> ERRO no request " + e.ToString() + "\r\n uri: " + uri);
+            }
+
+        }
+
+    }
+}
